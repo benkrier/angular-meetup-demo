fe998f61c8661170893df491423e448486436d61
Benjamin Krier <benkrier@gmail.com>
2019-08-10 13:21:34 -0500

    add methods and tests to text area component

--- src/app/dynamic-form/components/text-area/text-area.component.spec.ts
+++ src/app/dynamic-form/components/text-area/text-area.component.spec.ts
@@ -1,25 +1,55 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
+import {TextAreaComponent} from './text-area.component';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule, MatInputModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { TextAreaComponent } from './text-area.component';
+const MockGroup = new FormGroup({'testTextarea': new FormControl()});
+const MockFormConfig = {type: 'textAreaLg', label: 'Test Textarea', name: 'testTextarea'};
 
 describe('TextAreaComponent', () => {
   let component: TextAreaComponent;
   let fixture: ComponentFixture<TextAreaComponent>;
+  let componentEl: any; // debugElement and nativeElement are type any
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TextAreaComponent ]
+      declarations: [ TextAreaComponent ],
+      imports: [
+        MatFormFieldModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TextAreaComponent);
     component = fixture.componentInstance;
+    component.formConfig = MockFormConfig;
+    component.group = MockGroup;
+    componentEl = fixture.debugElement.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render textarea field with correct formConfig placeholder', () => {
+    const placeholder = componentEl.querySelector('.mat-form-field-label');
+    expect(placeholder.textContent.trim()).toBe('Test Textarea');
+  });
+
+  it('#triggerResize: should fire when value changes in textarea field', fakeAsync(() => {
+    spyOn(component, 'triggerResize');
+    const textArea = componentEl.querySelector('textarea[ng-reflect-name="testTextarea"]');
+    textArea.value = 'test code block value';
+    textArea.dispatchEvent(new Event('change'));
+    fixture.detectChanges();
+    expect(component.triggerResize).toHaveBeenCalled();
+  }));
 });
--- src/app/dynamic-form/components/text-area/text-area.component.ts
+++ src/app/dynamic-form/components/text-area/text-area.component.ts
@@ -1,13 +1,25 @@
-import { Component } from "@angular/core";
-import { FormGroup } from "@angular/forms";
-import { DynamicFormConfigObject } from "../../models/dynamic-form-config-object";
+import {Component, NgZone, ViewChild} from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import {Component, NgZone, OnInit, ViewChild} from '@angular/core';
+import {take} from 'rxjs/operators';
+import {FormGroup} from '@angular/forms';
+import {CdkTextareaAutosize} from '@angular/cdk/text-field';
+import {DynamicFormConfigObject} from '../../models/dynamic-form-config-object';
 
 @Component({
-  selector: "app-text-area",
-  templateUrl: "./text-area.component.html",
-  styleUrls: ["./text-area.component.scss"]
+  selector: 'app-text-area',
+  templateUrl: './text-area.component.html',
+  styleUrls: ['./text-area.component.scss']
 })
 export class TextAreaComponent {
   formConfig: DynamicFormConfigObject;
   group: FormGroup;
+  @ViewChild('autosize', { static: true }) autosize: CdkTextareaAutosize;
+
+  constructor(private ngZone: NgZone) { }
+
+  triggerResize() {
+    this.ngZone.onStable.pipe(take(1))
+      .subscribe(() => this.autosize.resizeToFitContent(true));
+  }
 }
