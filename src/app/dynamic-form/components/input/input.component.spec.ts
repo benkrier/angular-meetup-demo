41ea726a98e925ef453a5d212b151cea3a1777a1
Benjamin Krier <benkrier@gmail.com>
2019-08-10 12:31:14 -0500

    add tests to input component

--- src/app/dynamic-form/components/input/input.component.spec.ts
+++ src/app/dynamic-form/components/input/input.component.spec.ts
@@ -1,25 +1,46 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { InputComponent } from './input.component';
+import {MatFormFieldModule, MatInputModule} from '@angular/material';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+const MockGroup = new FormGroup({'testInput': new FormControl()});
+const MockFormConfig = {type: 'input', label: 'Test Input', name: 'testInput'};
 
 describe('InputComponent', () => {
   let component: InputComponent;
   let fixture: ComponentFixture<InputComponent>;
+  let componentEl: any; // debugElement and nativeElement are type any
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ InputComponent ]
+      declarations: [ InputComponent ],
+      imports: [
+        MatFormFieldModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(InputComponent);
     component = fixture.componentInstance;
+    componentEl = fixture.debugElement.nativeElement;
+    component.formConfig = MockFormConfig;
+    component.group = MockGroup;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render input field with correct formConfig label', () => {
+    const placeholder = componentEl.querySelector('.mat-form-field-label');
+    expect(placeholder.textContent.trim()).toBe('Test Input');
   });
 });
